7af91a0ea6e7da7e29159f663f5b089b8a6edc2b
Rik rogaar <rik@qlic.nl>
2024-01-11 10:46:20 +0100

    wip

--- framework/decorators/componentDecorator.ts
+++ framework/decorators/componentDecorator.ts
@@ -4,12 +4,12 @@ export default function Component(data: componentDecoratorParams) {
     return (ctr: any) => {
         const { template, styles, initialRender } = data;
         const instance = new ctr();
-        new ParserTest(template, styles, initialRender ?? '', instance);
+        new ParserTest(template, styles ?? '', initialRender ?? '', instance);
     }
 }
 
 interface componentDecoratorParams {
     template: string;
-    styles: string;
+    styles?: string;
     initialRender?: string;
 }
